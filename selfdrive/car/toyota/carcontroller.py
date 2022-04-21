e6343a8fc00348e55ffdfefb7f95c35c798898d7
sheldonroth <34106288+sheldonroth@users.noreply.github.com>
2022-04-21 15:14:58 -0600

    Update carcontroller.py

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -12,6 +12,8 @@ VisualAlert = car.CarControl.HUDControl.VisualAlert
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
+    self.CP = CP
+    self.torque_rate_limits = CarControllerParams(self.CP)
     self.last_steer = 0
     self.alert_active = False
     self.last_standstill = False
@@ -45,7 +47,7 @@ class CarController():
 
     # steer torque
     new_steer = int(round(actuators.steer * CarControllerParams.STEER_MAX))
-    apply_steer = apply_toyota_steer_torque_limits(new_steer, self.last_steer, CS.out.steeringTorqueEps, CarControllerParams)
+    apply_steer = apply_toyota_steer_torque_limits(new_steer, self.last_steer, CS.out.steeringTorqueEps, self.torque_rate_limits)
     self.steer_rate_limited = new_steer != apply_steer
 
     # Cut steering while we're in a known fault state (2s)
