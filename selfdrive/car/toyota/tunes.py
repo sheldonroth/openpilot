191ac46033beb8142e74ddd4c41f32073805ef50
sheldonroth <34106288+sheldonroth@users.noreply.github.com>
2022-04-21 20:59:57 -0600

    Update tunes.py

--- selfdrive/car/toyota/tunes.py
+++ selfdrive/car/toyota/tunes.py
@@ -57,6 +57,7 @@ def set_lat_tune(tune, name, MAX_TORQUE=2.5, FRICTION=.1):
      tune.torque.kf = 1.0 / MAX_TORQUE
      tune.torque.ki = 0.5 / MAX_TORQUE
      tune.torque.friction = FRICTION
+
   elif name == LatTunes.INDI_PRIUS:
     tune.init('indi')
     tune.indi.innerLoopGainBP = [0.]
