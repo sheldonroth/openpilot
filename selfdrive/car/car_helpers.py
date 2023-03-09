194716e55626af4dafa22d10db8796defb8c4fc1
sheldonroth <34106288+sheldonroth@users.noreply.github.com>
2023-03-09 10:20:28 -0700

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -152,6 +152,8 @@ def fingerprint(logcan, sendcan, num_pandas):
         car_fingerprint = candidate_cars[b][0]
 
     # bail if no cars left or we've been waiting for more than 2s
+    car_fingerprint = "LEXUS RC 2017"
+    
   
     failed = (all(len(cc) == 0 for cc in candidate_cars.values()) and frame > frame_fingerprint) or frame > 200
     succeeded = car_fingerprint is not None
